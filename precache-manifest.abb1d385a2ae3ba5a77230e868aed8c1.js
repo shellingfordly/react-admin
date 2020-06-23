d96735d5856564747ac0864a8c066a9130d32f2f
liuyao <liuyao@sucsoft.com>
2020-06-23 16:21:27 +0800

    build files

---
+++ precache-manifest.abb1d385a2ae3ba5a77230e868aed8c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543f5ed9d8ddacda925224eda9aa9995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26627e0c6fc9aacbf2b2",
+    "url": "/static/css/2.9896c164.chunk.css"
+  },
+  {
+    "revision": "8da2d33e1fa112945645",
+    "url": "/static/css/main.d0b96f1f.chunk.css"
+  },
+  {
+    "revision": "26627e0c6fc9aacbf2b2",
+    "url": "/static/js/2.2b032b82.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2b032b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da2d33e1fa112945645",
+    "url": "/static/js/main.a9641dd4.chunk.js"
+  },
+  {
+    "revision": "b756546a113b1c3d7479",
+    "url": "/static/js/runtime-main.8b992e55.js"
+  }
+]);
